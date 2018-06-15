8097406028e7d1fe4ab36b86e5261822fdf0c852
SubasishMohapatra <subasish.mohapatra@daimler.com>
2018-06-15 10:01:07 +0530

    Sequential duplex communication

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -4,6 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.IO.Pipes;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Security.Principal;
 
 namespace ClientApp
 {
@@ -11,32 +14,92 @@ namespace ClientApp
     {
         static void Main(string[] args)
         {
-            using (NamedPipeClientStream namedPipeClient = new NamedPipeClientStream("CGSCLRD48139536", "ClientServerPipe"))
+            using (NamedPipeClientStream namedPipeClientStream = new NamedPipeClientStream(".", "ClientServerPipe",
+                                PipeDirection.InOut, PipeOptions.None,
+                                TokenImpersonationLevel.Impersonation))
             {
-                try
-                {
-                    Console.WriteLine("Enter any key(escape to exit)");
-                    var number = 0;
-                    while (Console.ReadKey().Key != ConsoleKey.Escape)
-                    {
-                        var applicationMessage = new ApplicationMessage()
-                        {
-                            SyncTime = DateTime.Now,
-                            SyncIDs = new List<int>() { ++number, ++number, ++number }
-                        };
-                        string serialised = JsonConvert.SerializeObject(applicationMessage);
-                        if (!namedPipeClient.IsConnected)
-                            namedPipeClient.Connect();
-                        byte[] messageBytes = Encoding.UTF8.GetBytes(serialised);
-                        namedPipeClient.Write(messageBytes, 0, messageBytes.Length);
-                        Console.WriteLine("Enter any key(escape to exit)");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Eror{ex.Message}");
-                }
+                SendMessage(namedPipeClientStream, "Hello 1");
+                Console.WriteLine($"Received from server {GetMessage(namedPipeClientStream)}");
+                SendMessage(namedPipeClientStream, "Hello 2");
+                Console.WriteLine($"Received from server {GetMessage(namedPipeClientStream)}");
+                SendMessage(namedPipeClientStream, "Hello 3");
+                Console.WriteLine($"Received from server {GetMessage(namedPipeClientStream)}");
             }
+            Console.ReadLine();
         }
+
+        static void SendMessage(NamedPipeClientStream namedPipeClient, string message)
+        {
+            if (!namedPipeClient.IsConnected)
+            {                
+                namedPipeClient.Connect();
+                namedPipeClient.ReadMode = PipeTransmissionMode.Message;
+            }
+            string serialised = JsonConvert.SerializeObject(message);
+            byte[] messageBytes = Encoding.UTF8.GetBytes(serialised);
+            namedPipeClient.Write(messageBytes, 0, messageBytes.Length);
+            namedPipeClient.Flush();
+        }
+
+        static string GetMessage(NamedPipeClientStream namedPipeClient)
+        {
+            if (!namedPipeClient.IsConnected)
+            {
+                namedPipeClient.Connect();
+            }
+            StringBuilder messageBuilder = new StringBuilder();
+            string messageChunk = string.Empty;
+            byte[] messageBuffer = new byte[1024];
+            do
+            {
+                namedPipeClient.Read(messageBuffer, 0, messageBuffer.Length);
+                messageChunk = Encoding.UTF8.GetString(messageBuffer);
+                messageBuilder.Append(messageChunk);
+                messageBuffer = new byte[messageBuffer.Length];
+            } while (!namedPipeClient.IsMessageComplete);
+            return JsonConvert.DeserializeObject<String>(messageBuilder.ToString());
+        }
+
+        #region Commented code
+
+        //        using (NamedPipeClientStream namedPipeClientStream = new NamedPipeClientStream(".", "ClientServerPipe",
+        //                        PipeDirection.InOut, PipeOptions.None,
+        //                        TokenImpersonationLevel.Impersonation))
+        //            {
+        //                try
+        //                {
+
+        //                    ThreadPool.QueueUserWorkItem((x) => ServerListener.Start(x), namedPipeClientStream);
+        //                    Console.WriteLine("Enter any key(escape to exit)");
+        //                    var number = 0;
+        //                    while (Console.ReadKey().Key != ConsoleKey.Escape)
+        //                    {
+        //                        var applicationMessage = new ApplicationMessage()
+        //                        {
+        //                            SyncTime = DateTime.Now,
+        //                            SyncIDs = new List<int>() { ++number, ++number, ++number }
+        //                        };
+        //    string serialised = JsonConvert.SerializeObject(applicationMessage);
+        //                        if (!namedPipeClientStream.IsConnected)
+        //                        {
+        //                            namedPipeClientStream.ReadMode = PipeTransmissionMode.Message;
+        //                            namedPipeClientStream.Connect();
+        //                        }
+        //byte[] messageBytes = Encoding.UTF8.GetBytes(serialised);
+        //Task.Run(async () =>
+        //                        {
+        //                            await namedPipeClientStream.WriteAsync(messageBytes, 0, messageBytes.Length);
+        //await namedPipeClientStream.FlushAsync();
+        //                        }).Wait();
+        //Console.WriteLine("Enter any key(escape to exit)");
+        //                    }
+        //                }
+        //                catch (Exception ex)
+        //                {
+        //                    Console.WriteLine($"Eror{ex.Message}");
+        //                }
+        //            }
+
+        #endregion
     }
 }
--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO.Pipes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,17 +13,54 @@ namespace ServerApp
     {
         static void Main(string[] args)
         {
-            StartClient();
+
+            using (var namedPipeServer =
+      new NamedPipeServerStream("ClientServerPipe", PipeDirection.InOut, 1, PipeTransmissionMode.Message))
+            {
+                if (!namedPipeServer.IsConnected)
+                {
+                    namedPipeServer.WaitForConnection();
+                    Console.WriteLine("Client connected");
+                }
+                var message = GetMessage(namedPipeServer);
+                Console.WriteLine($"From client: {message}");
+                SendMessage(namedPipeServer, $"Received {message}");
+                message = GetMessage(namedPipeServer);
+                Console.WriteLine($"From client: {message}");
+                SendMessage(namedPipeServer, $"Received {message}");
+                message = GetMessage(namedPipeServer);
+                Console.WriteLine($"From client: {message}");
+                SendMessage(namedPipeServer, $"Received {message}");
+            }
+            Console.ReadLine();
         }
 
-        private static void StartClient()
+        static void SendMessage(NamedPipeServerStream namedPipeServer, string message)
         {
-            //Process[] processes = Process.GetProcessesByName("ClientApp");
-            //if (processes != null && processes.Count() <= 0)
-            //{
-            //    var autoSync = Process.Start("ClientApp.exe");
-                ClientListener.Start();
-            //}
+            var serialized = JsonConvert.SerializeObject(message);
+            byte[] messageBytes = Encoding.UTF8.GetBytes(serialized);
+            if (!namedPipeServer.IsConnected)
+            {
+                namedPipeServer.WaitForConnection();
+                Console.WriteLine("Client connected");
+            }
+            namedPipeServer.Write(messageBytes, 0, messageBytes.Length);
+            namedPipeServer.Flush();
+        }
+
+        static string GetMessage(NamedPipeServerStream namedPipeServer)
+        {
+            StringBuilder messageBuilder = new StringBuilder();
+            string messageChunk = string.Empty;
+            byte[] messageBuffer = new byte[1024];
+            do
+            {
+                namedPipeServer.Read(messageBuffer, 0, messageBuffer.Length);
+                messageChunk = Encoding.UTF8.GetString(messageBuffer);
+                messageBuilder.Append(messageChunk);
+                messageBuffer = new byte[messageBuffer.Length];
+            } while (!namedPipeServer.IsMessageComplete);
+            return JsonConvert.DeserializeObject<string>(messageBuilder.ToString());
         }
     }
 }
