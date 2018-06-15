0dad222708b9cfbd96bc4da3dc36de573bc079c8
SubasishMohapatra <subasish.mohapatra@daimler.com>
2018-06-15 10:01:55 +0530

    Demonstrate duplex communication using named pipes
    Duplex communicatiion using named pipes

--- ServerApp/ClientListener.cs
+++ ServerApp/ClientListener.cs
@@ -6,12 +6,13 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.IO;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ServerApp
 {
     internal class ClientListener
     {
-        internal static void Start()
+        internal static void Start(Action<ApplicationMessage, NamedPipeServerStream> callBack)
         {
             using (var namedPipeServer =
          new NamedPipeServerStream("ClientServerPipe", PipeDirection.InOut, 1, PipeTransmissionMode.Message))
@@ -30,15 +31,14 @@ namespace ServerApp
                         byte[] messageBuffer = new byte[1024];
                         do
                         {
-                            namedPipeServer.Read(messageBuffer, 0, messageBuffer.Length);
+                            Task.Run(async () => await namedPipeServer.ReadAsync(messageBuffer, 0, messageBuffer.Length));
                             messageChunk = Encoding.UTF8.GetString(messageBuffer);
                             messageBuilder.Append(messageChunk);
                             messageBuffer = new byte[messageBuffer.Length];
                         } while (!namedPipeServer.IsMessageComplete);
                         var applicationMessage = JsonConvert.DeserializeObject<ApplicationMessage>(messageBuilder.ToString());
-                        if (applicationMessage != null)
-                            Console.WriteLine($"Sync Time:{applicationMessage.SyncTime}\n" +
-                                $"Sync ID's received:{String.Join<int>(",", applicationMessage.SyncIDs)}");
+                        callBack(applicationMessage, namedPipeServer);
+
                     }
                 }
                 catch (Exception ex)
@@ -48,9 +48,33 @@ namespace ServerApp
                     else
                         Console.WriteLine("ERROR: {0}", ex.Message);
                     namedPipeServer.Disconnect();
+                    //namedPipeServer.Close();
                 }
             }
-            Start();
+            Start(callBack);
+        }
+
+        internal static void ProcesClientMessage(ApplicationMessage applicationMessage, NamedPipeServerStream namedPipeServer)
+        {
+            if (applicationMessage != null)
+            {
+                var printMessage = $"Sync Time:{applicationMessage.SyncTime}\n" +
+                    $"Sync ID's received:{String.Join<int>(",", applicationMessage.SyncIDs)}";
+                Console.WriteLine($"Message from client:\n{printMessage}");
+                var serialized = JsonConvert.SerializeObject(printMessage);
+                byte[] messageBytes = Encoding.UTF8.GetBytes(serialized);
+                if (!namedPipeServer.IsConnected)
+                {
+                    namedPipeServer.WaitForConnection();
+                    Console.WriteLine("Client connected");
+                }
+                Task.Run(async () =>
+                {
+                    await namedPipeServer.WriteAsync(messageBytes, 0, messageBytes.Length);
+                    await namedPipeServer.FlushAsync();
+                }
+                ).Wait();
+            }
         }
     }
 }
